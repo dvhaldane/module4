b6ea86316ac28814ea5d14620e423dbd3a391b6c
dvhaldane <dvhaldane@me.com>
2017-09-24 11:36:08 -0500

    content update

---
+++ test/test.java
@@ -0,0 +1,16 @@
+import org.junit.Test;
+
+/**
+ * Created by HaldaneDavidV on 9/24/2017.
+ */
+public class test extends CommonElements
+{
+    @Test
+    void test1()
+    {
+        Comparable[] query_Collection = {1,2,3,4};
+        Comparable[] current_Collection = {1,2,3,4};
+        CommonElements comparison = new CommonElements().findCommonElements();
+
+    }
+}
