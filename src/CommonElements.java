bad84f8b2cd1c39d05c07961993ab1847d3f9d84
dvhaldane <dvhaldane@me.com>
2017-09-24 11:35:41 -0500

    content update

---
+++ src/CommonElements.java
@@ -0,0 +1,45 @@
+/**
+ * Created by HaldaneDavidV on 9/24/2017.
+ */
+public class CommonElements
+{
+    int comparisons;
+
+    CommonElements()
+    {
+        comparisons = 0;
+    }
+
+    public Comparable[] findCommonElements(Comparable[][] collections)
+    {
+        Comparable[] commonElements = {};
+
+        //Sort
+
+        //Does array have at least 10 entries?
+            //Yes, use Quick Sort
+
+            //No, use insertion sort
+
+        //Compare
+
+
+        return commonElements;
+
+    }
+
+    public Comparable[][] insertionSortRemoveDuplicates(Comparable[][] collections)
+    {
+        return collections;
+    }
+
+    public Comparable[][] quickSortRemoveDuplicates(Comparable[][] collections)
+    {
+        return collections;
+    }
+
+    public int getComparisons()
+    {
+        return this.comparisons;
+    }
+}
