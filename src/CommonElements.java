73b73b16f9a80c875809f2c3326cd3e6e65edc05
dvhaldane <dvhaldane@me.com>
2017-09-24 22:49:25 -0500

    Add sizing logic and linear comparison.

--- src/CommonElements.java
+++ src/CommonElements.java
@@ -1,3 +1,5 @@
+import java.util.ArrayList;
+
 /**
  * Created by HaldaneDavidV on 9/24/2017.
  */
@@ -13,37 +15,63 @@ public class CommonElements
     public Comparable[] findCommonElements(Comparable[][] collections)
     {
         //Placeholder to declare return value
-        Comparable[] commonElements = {};
+        ArrayList<Comparable> commonElementsList = new ArrayList<>();
 
         //Tempcomparable to store sorted arrays
-        Comparable[][] tempComparable = {};
+        Comparable[][] tempCollections = collections;
+
+        ArrayList<Integer> collectionLengths = new ArrayList<Integer>();
 
         //Iterate through array of arrays to sort
         for (int i = 0; i < collections.length; i++)
         {
             if (collections[i].length >= 10)
             {
-                tempComparable[i] = quickSortRemoveDuplicates(collections[i]);
+                tempCollections[i] = quickSort(collections[i]);
             }
             else
             {
-                tempComparable[i] = insertionSortRemoveDuplicates(collections[i]);
+                tempCollections[i] = insertionSort(collections[i]);
+            }
+        }
+
+        //Find largest array for query array
+        Comparable[] temp;
+
+        for (int i = 1; i < tempCollections.length; i++)
+        {
+            if (tempCollections[i].length > tempCollections[0].length)
+            {
+                temp = tempCollections[0];
+                tempCollections[0] = tempCollections[i];
+                tempCollections[i] = temp;
             }
         }
 
         //Compare
+        Comparable[] queryCollection = tempCollections[0];
 
+        for (int i = 1; i < tempCollections[i].length; i++)
+        {
+            comparisons++;
+            if (queryCollection[i].compareTo(tempCollections[i]) == 0)
+            {
+                commonElementsList.add(queryCollection[i]);
+            }
+        }
+
+        Comparable[] commonElements = commonElementsList.toArray(new Comparable[commonElementsList.size()]);
 
         return commonElements;
 
     }
 
-    public Comparable[] insertionSortRemoveDuplicates(Comparable[] collections)
+    public Comparable[] insertionSort(Comparable[] collections)
     {
         return collections;
     }
 
-    public Comparable[] quickSortRemoveDuplicates(Comparable[] collections)
+    public Comparable[] quickSort(Comparable[] collections)
     {
         return collections;
     }
