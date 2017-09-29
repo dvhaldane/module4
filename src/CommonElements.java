a58229331214864c15e20074c7662b3a9e430764
dvhaldane <dvhaldane@me.com>
2017-09-28 21:21:07 -0500

    Duplicate handling additions.

--- src/CommonElements.java
+++ src/CommonElements.java
@@ -19,7 +19,7 @@ public class CommonElements
     public Comparable[] findCommonElements(Comparable[][] collections)
     {
         //queryCollection = commonElementsList.toArray(new Comparable[commonElementsList.size()]);
-        ArrayList<Comparable> commonElementsList = new ArrayList<>();
+
         ArrayList<Comparable[]> nonNullArrays = new ArrayList<>();
 
         // Remove Nulls
@@ -57,12 +57,60 @@ public class CommonElements
                 tempCollections[0] = tempCollections[i];
                 tempCollections[i] = temp;
             }
+        }
 
+        //Compress Duplicates
+        ArrayList<Comparable> compressedArray = new ArrayList<>();
+        for (int i = 0; i < tempCollections.length; i++)
+        {
+            Comparable[] currArray = tempCollections[i];
+            int currCount = 1;
 
+            for (int j = 0; j < tempCollections[i].length; j++)
+            {
+                if (currArray.length > 1)
+                {
+                    if (j < currArray.length - 1)
+                    {
+                        if (currArray[j].compareTo(currArray[j + 1]) == 0)
+                        {
+                            currCount++;
+                        } else
+                        {
+                            compressedArray.add(currArray[j]);
+                            compressedArray.add(currCount);
+                            currCount = 1;
+                        }
+                    }
+                    else
+                    {
+                        if (currArray[j].compareTo(currArray[j - 1]) == 0)
+                        {
+                            compressedArray.add(currArray[j]);
+                            compressedArray.add(currCount);
+                            currCount = 1;
+                        }
+                        else
+                        {
+                            compressedArray.add(currArray[j]);
+                            compressedArray.add(currCount);
+                            currCount = 1;
+                        }
+                    }
+                } else
+                {
+                    compressedArray.add(currArray[0]);
+                    compressedArray.add(currCount);
+                    currCount = 1;
+                }
+            }
 
+            tempCollections[i] = compressedArray.toArray(new Comparable[compressedArray.size()]);
+            compressedArray.clear();
         }
 
         //Compare
+        ArrayList<Comparable> commonElementsList = new ArrayList<>();
         int currentArrayIndex = 1;
         Comparable[] queryCollection = tempCollections[0];
         Comparable[] currentCompare = tempCollections[currentArrayIndex];
@@ -83,34 +131,36 @@ public class CommonElements
             }
             if (compareValue == 0)
             {
-                if (j < queryCollection.length && k < currentCompare.length)
+                if (j < queryCollection.length - 1 && k < currentCompare.length - 1)
                 {
                     commonElementsList.add(queryCollection[j]);
+                    commonElementsList.add(Math.abs((int)queryCollection[j + 1] - (int)queryCollection[k + 1]));
                 }
 
-                if (k == currentCompare.length - 1)
+                if (k == currentCompare.length - 2)
                 {
-                    j++;
+                    j+=2;
                 }
                 else
                 {
-                    j++;
-                    k++;
+                    j+=2;
+                    k+=2;
                 }
 
             }
-            else if (compareValue < 0 && j < queryCollection.length - 1) //If query is behind the current compare and not at end or at end
+            else if (compareValue < 0 && j < queryCollection.length - 2) //If query is behind the current compare and not at end or at end
             {
-                j++;
+                j+=2;
             }
-            else if (compareValue > 0 && k < currentCompare.length - 1) //If query is ahead of comparison and comparison is not at end or at end
+            else if (compareValue > 0 && k < currentCompare.length - 2) //If query is ahead of comparison and comparison is not at end or at end
             {
-                k++;
+                k+=2;
             }
-            if (j == queryCollection.length || j == queryCollection.length - 1 && compareValue < 0 || k == currentCompare.length - 1 && compareValue > 0 || k == currentCompare.length && j == queryCollection.length)
+            if (j == queryCollection.length || j == queryCollection.length - 2 && compareValue < 0 || k == currentCompare.length - 2 && compareValue > 0 || k == currentCompare.length && j == queryCollection.length)
             {
                 queryCollection = commonElementsList.toArray(new Comparable[commonElementsList.size()]);
                 commonElementsList.clear();
+
                 j = 0;
                 k = 0;
                 currentArrayIndex++;
@@ -122,8 +172,36 @@ public class CommonElements
         }
 
         arrayLength = queryCollection.length;
-        return queryCollection;
+        return processArrayListDuplicates(commonElementsList);
 
+    }
+
+    public Comparable[] processArrayListDuplicates(ArrayList<Comparable> arrayList)
+    {
+        int count = 1;
+        ArrayList<Comparable> returnList = new ArrayList<>();
+
+        for (Comparable c : arrayList)
+        {
+            if (count % 2 == 1)
+            {
+                returnList.add(c);
+            }
+            else
+            {
+                int duplicateCount = (Integer) c;
+
+                if (duplicateCount > 1)
+                {
+                    for (int i = 0; i < duplicateCount; i++)
+                    {
+                        returnList.add(returnList.get(returnList.size() - 1));
+                    }
+                }
+            }
+        }
+
+        return returnList.toArray(new Comparable[returnList.size()]);
     }
 
     public Comparable[] insertionSort(Comparable[] collections)
